218ad8424f759a6749f97e4c954df6c62c920e7a
ansivgit <ansiv@yandex.ru>
2022-10-24 21:34:32 +0300

    fix: integration FE with DB

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -6,10 +6,10 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { formatAsPrice } from "~/utils/utils";
 import AddProductToCart from "~/components/AddProductToCart/AddProductToCart";
-import { useAvailableProducts } from "~/queries/products";
+import { useAllProducts } from "~/queries/products";
 
 export default function Products() {
-  const { data = [], isLoading } = useAvailableProducts();
+  const { data = [], isLoading } = useAllProducts();
 
   if (isLoading) {
     return <Typography>Loading...</Typography>;
@@ -17,22 +17,29 @@ export default function Products() {
 
   return (
     <Grid container spacing={4}>
-      {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-      {data.map(({ count, ...product }, index) => (
+      {data.map(({ count, ...product }) => (
         <Grid item key={product.id} xs={12} sm={6} md={4}>
           <Card
             sx={{ height: "100%", display: "flex", flexDirection: "column" }}
           >
             <CardMedia
               sx={{ pt: "56.25%" }}
-              image={`https://source.unsplash.com/random?sig=${index}`}
+              image={product.image}
               title="Image title"
             />
             <CardContent sx={{ flexGrow: 1 }}>
               <Typography gutterBottom variant="h5" component="h2">
                 {product.title}
               </Typography>
-              <Typography>{formatAsPrice(product.price)}</Typography>
+              <Typography variant="h6" color="text.secondary" component="h2">
+                {product.description}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {formatAsPrice(product.price)}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {count && `In stock: ${count}`}
+              </Typography>
             </CardContent>
             <CardActions>
               <AddProductToCart product={product} />
