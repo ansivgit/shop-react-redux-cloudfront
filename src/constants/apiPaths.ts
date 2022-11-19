2c2b0e36bea8615949e186ffc116a32a5345a08a
ansivgit <ansiv@yandex.ru>
2022-11-19 18:40:08 +0300

    change path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const PRODUCT_SERVICE_HOST_URL =
   "https://m0n1i622y2.execute-api.eu-west-1.amazonaws.com/dev";
 const IMPORT_SERVICE_HOST_URL =
-  "https://2cjm8tds5e.execute-api.eu-west-1.amazonaws.com/dev";
+  "https://fe9oh4ghg3.execute-api.eu-west-1.amazonaws.com/dev";
 
 const API_PATHS = {
   products: `${PRODUCT_SERVICE_HOST_URL}/products`,
